d43a10d3f0a3cb317b5ed9842fa9ddcf4e6e1a6c
dangsh <954316227@qq.com>
2017-11-14 15:23:09 +0800

    this is a blog by flask and python3

---
+++ myStorage/mySql.sql
@@ -0,0 +1,19 @@
+drop table if exists productTable;
+
+create table productTable(
+    id integer primary key autoincrement , 
+    aftersale string ,
+    businessId string ,
+    depictandimage string ,
+    flag string ,
+    freight string ,
+    imgurl string ,
+    norm string ,
+    price1 string ,
+    productid string ,
+    productdepict string ,
+    productname string ,
+    productsortid string ,
+    shop string
+
+)
