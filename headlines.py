7c06a6d7b9a978c61d99057b11051f4e60d2647e
dangsh <954316227@qq.com>
2018-02-02 15:58:35 +0800

    update all static file in 2_2

--- headlines.py
+++ headlines.py
@@ -5,63 +5,21 @@ import datetime
 import requests
 import feedparser
 from flask import Flask, render_template, request, make_response
-from song import getMusicInfo
 import linkdb
 import json
 
 app = Flask(__name__)
 
-RSS_FEED = {"zhihu": "https://www.zhihu.com/rss",
-            "netease": "http://news.163.com/special/00011K6L/rss_newsattitude.xml",
-            "songshuhui": "http://songshuhui.net/feed",
-            "t1": "http://news.qq.com/newsgn/rss_newsgn.xml" ,
-            "t2": "http://news.qq.com/newsgj/rss_newswj.xml",
-            "s1":"http://rss.sina.com.cn/news/china/focus15.xml" ,
-            "s2":"http://rss.sina.com.cn/news/world/focus15.xml" ,
-            "fankexue" : "http://pansci.asia/feed" ,
-            "r1" : "http://www.people.com.cn/rss/politics.xml" ,
-            "r2" : "http://www.people.com.cn/rss/world.xml" ,
-            "w1" : "http://news.163.com/special/00011K6L/rss_newstop.xml" ,
-            "k1" : "http://www.sciencenet.cn/xml/news-0.aspx?news=0",
-            "k2" : "http://www.sciencenet.cn/xml/news-0.aspx?di=0" ,
-            "k3" : "http://www.sciencenet.cn/xml/news-0.aspx?di=1" ,
-            "k4" : "http://www.sciencenet.cn/xml/news-0.aspx?di=7" ,
-            "souhu1" : "http://rss.news.sohu.com/rss/guonei.xml" ,
-            "souhu2" : "http://rss.news.sohu.com/rss/guoji.xml"
-            }
 
-DEFAULTS = {'publication': 'souhu2'}
-
-
-
-def get_value_with_fallback(key):
-    if request.args.get(key):
-        return request.args.get(key)
-    if request.cookies.get(key):
-        return request.cookies.get(key)
-    return DEFAULTS[key]
 
 @app.route('/')
 def index():
-    return render_template("index3.html")
+    return render_template("index.html")
 
 @app.errorhandler(404)
 def lostPage(err):
-    # return render_template("lostPage.html")
-    # return redirect(url_for("indexFn"))
-    return "我是404界面"
+    return render_template("404.html")
 
-@app.route('/about')
-def aboutFn():
-    return render_template("about.html")
-
-@app.route('/top')
-def home():
-    publication = get_value_with_fallback('publication')
-    articles = get_news(publication) #获得头条数据
-    response = make_response(render_template('top.html', articles=articles,))
-    response.set_cookie('publication',  publication)
-    return response
 
 @app.route('/delSpace')
 def delSpaceFn():
@@ -71,18 +29,6 @@ def delSpaceFn():
 def delCharFn():
     return render_template("delChar.html")
 
-@app.route('/music')
-def musicFn():
-    name = get_value_with_fallback('publication')
-    info  = getMusicInfo(name)
-    response = make_response(render_template('music.html' , info=info))
-    return response
-
-@app.route('/musicImp')
-def musicImpFn():
-    music_info  = getMusicInfo('周星驰')
-    response = make_response(render_template('music.html'))
-    return response
 
 @app.route('/base')
 def baseFn():
@@ -101,6 +47,20 @@ def loginCheckFn():
         return render_template("manage.html")
     else:
         return render_template("login.html")
+
+@app.route('/forgot')
+def forgotFn():
+    return render_template("forgot.html")
+
+@app.route('/sign')
+def signFn():
+    return render_template("sign.html")
+
+@app.route('/messageboard')
+def messageboardFn():
+    return render_template("messageBoard.html")
+
+
 
 @app.route('/addmsg' , methods=['GET' , 'POST'])
 def addmsgFn():
@@ -131,16 +91,13 @@ def oneSelectFn():
 def msgDelFn():
     id = request.args.get('id') 
     linkdb.msgDel(id)
-    return "删除成功"
+    return render_template("manage.html")
     
 @app.route('/content' , methods=['GET', 'POST'])
 def contentFn():
     id = request.args.get('id')
     return render_template("content.html")
 
-def get_news(publication):
-    feed = feedparser.parse(RSS_FEED[publication])
-    return feed['entries']
 
 
 
